f8b1fa1b1a5681deb08c36c4c946d123af451c6f
xlivix <oliviabryanttt@gmail.com>
2023-06-14 23:57:14 -0400

    update

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,89 @@
+const Flask = {
+    render_template: (template, data) => {
+        const templateHTML = document.getElementById(template).innerHTML;
+        const renderedHTML = Mustache.render(templateHTML, data);
+        document.body.innerHTML = renderedHTML;
+    },
+    request: {
+        args: {
+            get: (key, defaultValue) => {
+                const urlParams = new URLSearchParams(window.location.search);
+                const value = urlParams.get(key);
+                return value !== null ? value : defaultValue;
+            },
+        },
+    },
+    jsonify: (data) => JSON.stringify(data),
+};
+
+async function fetchData(query) {
+    const response = await fetch(`/data?query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data;
+}
+
+function loadCSVToDatabase() {
+    // Create a SQLAlchemy engine to connect to the database
+    const engine = {
+        execute: (query) => fetchData(query),
+    };
+
+    const csvFiles = ['players_15', 'players_16', 'players_17', 'players_18', 'players_19', 'players_20'];
+
+    for (const file of csvFiles) {
+        const tableName = file;
+        const query = `SELECT * FROM ${tableName} LIMIT 100`;
+        fetchData(query)
+            .then((data) => {
+                const players = data;
+                console.log(`Loaded ${players.length} records from ${tableName}`);
+            })
+            .catch((error) => {
+                console.error(`Failed to load data from ${tableName}`, error);
+            });
+    }
+}
+
+loadCSVToDatabase();
+
+function getPlayerInfo() {
+    const selectedPlayer = document.getElementById('player-select').value;
+    if (selectedPlayer !== '') {
+        const query = `SELECT short_name, age, nationality, club FROM players WHERE short_name = '${selectedPlayer}'`;
+        fetchData(query)
+            .then((data) => {
+                const player = data[0];
+                const tableBody = document.getElementById('player-table').getElementsByTagName('tbody')[0];
+                tableBody.innerHTML = `
+                    <tr>
+                        <td>${player.short_name}</td>
+                        <td>${player.age}</td>
+                        <td>${player.nationality}</td>
+                        <td>${player.club}</td>
+                    </tr>
+                `;
+            })
+            .catch((error) => {
+                console.error('Failed to fetch player data:', error);
+            });
+    }
+}
+
+$(document).ready(function() {
+    // Fetch the data for the initial page load
+    const query = 'SELECT * FROM players LIMIT 100';
+    fetchData(query)
+        .then((data) => {
+            const players = data;
+            const columns = Object.keys(players[0]);
+            Flask.render_template('index.html', { players, columns });
+        })
+        .catch((error) => {
+            console.error('Failed to fetch data:', error);
+        });
+});
+
+/*
 $(document).ready(function() {
     // Add active class to the first table link by default
     $(".filters a:first").addClass("active");
@@ -47,3 +133,4 @@ function updateTable(data) {
     });
     $("table tbody").html(tableRows);
 }
+*/
