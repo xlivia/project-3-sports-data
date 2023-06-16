c1ced6689d2bef9e4210406d59bd19086d18a96b
xlivix <oliviabryanttt@gmail.com>
2023-06-16 11:49:36 -0400

    update

--- static/js/map.js
+++ static/js/map.js
@@ -1,5 +1,5 @@
 function displayMap() {
-    var map = L.map('map', { attributionControl: false }).setView([51.505, -0.09], 13);
+    var map = L.map('map', { attributionControl: false }).setView([51.505, -0.09], 2);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         //attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
         maxZoom: 18,
